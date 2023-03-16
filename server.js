131d310fa0245f94c6ff94dbf0c26906e20389d0
MacKetchup <stylesthestyer@gmail.com>
2023-03-15 21:39:39 -0700

    wired routeApi, routeHtml to server.js file

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const path = require('path');
 // Initializing
 const app = express();
 const PORT = process.env.PORT || 3001;
+const routeApi = require('./routes/routeApi');
+const routeHtml = require('./routes/routeHtml');
 // Setting up Data Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-// Requiring route.js file
-require('./routes/routes')(app);
+app.use('/api', routeApi);
+app.use('/', routeHtml);
 // PORT Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: 🏍️ " + PORT);
